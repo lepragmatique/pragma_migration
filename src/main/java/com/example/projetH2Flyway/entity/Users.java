5994d7911fe70b75868436203c4b39d83a0d95f4
Tchao Franki <joelxavif@gmail.com>
2023-05-30 12:40:16 +0200

    modification du fichier Users.java

--- src/main/java/com/example/projetH2Flyway/entity/Users.java
+++ src/main/java/com/example/projetH2Flyway/entity/Users.java
@@ -9,7 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.util.Date;
+
 
 /**
  *
@@ -26,7 +26,7 @@ public class Users {
     private String firstName;
     private String lastName;
     private String email;
-    private String adresse;
+    private String password;
     
     
     

e8ecfb3f9a9549640b99a730a49877d7c44d8568
Tchao Franki <joelxavif@gmail.com>
2023-05-30 12:09:11 +0200

    ajout des fichier désiré

---
+++ src/main/java/com/example/projetH2Flyway/ProjetH2FlywayApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetH2Flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetH2FlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetH2FlywayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetH2Flyway/ProjetH2FlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetH2Flyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetH2FlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
